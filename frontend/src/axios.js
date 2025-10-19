ec1b3e845a700316473bc482bfa7bfd76334106b
Nukala Hanumath Rakesh <rakeshcoc1to11@example.com>
2025-10-19 19:58:34 +0530

    completed

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api",
   withCredentials: true,
+  headers: { "Content-Type": "application/json" }
 });
-
-export default api;
